6631be372a062fb0474273a412b24cf72d7837d5
iceulf <den@iceulf.dev>
2026-06-30 09:01:56 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> result = new HashMap<>();
+
+        for (String s: strs) {
+            var key = sort(s);
+            List<String> list = new ArrayList<String>();
+            if (result.containsKey(key)) {
+                list = result.get(key);
+            }
+            list.add(s);
+            result.put(key, list);
+        }
+
+        return result.values().stream().toList();
+    }
+
+    private String sort(String s) {
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 97]++;
+        }
+        String result = "";
+        for (int i = 0; i < freq.length; i++) {
+            for (int j = 0; j < freq[i]; j++) {
+                result += (char) i + 97;
+            }
+        }
+        return result;
+    }
+}
