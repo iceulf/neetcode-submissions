5c2b44b9901ece87965cb2b1a84c5d21e85f03cf
iceulf <den@iceulf.dev>
2026-06-30 08:24:59 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> freq = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char sc = s.charAt(i);
+            char tc = t.charAt(i);
+
+            if (sc == tc) continue;
+
+            freq.put(sc, freq.getOrDefault(sc, 0) + 1);
+            freq.put(tc, freq.getOrDefault(tc, 0) - 1);
+        }
+
+        for (int c: freq.values()) {
+            if (c != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
