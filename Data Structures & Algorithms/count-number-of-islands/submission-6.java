a1d5c0b12051918874319dcad254d52bc6b31fef
iceulf <den@iceulf.dev>
2026-07-09 15:38:31 +0530

    Add: count-number-of-islands - submission-6

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-6.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        var visited = new HashSet<String>();
+        var result = 0;
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1' && dfs(grid, new int[]{i, j}, visited)) {
+                    result += 1;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private boolean dfs(char[][] grid, int[] start, Set<String> visited) {
+        var row = start[0]; 
+        var col = start[1];
+        var pos = row + ":" + col;
+
+        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0') {
+            return false;
+        } 
+
+        if (visited.contains(pos)) {
+            return false;
+        }
+
+        visited.add(pos);
+
+        dfs(grid, new int[]{row, col - 1}, visited);
+        dfs(grid, new int[]{row, col + 1}, visited);
+        dfs(grid, new int[]{row + 1, col}, visited);
+        dfs(grid, new int[]{row - 1, col}, visited);
+
+        return true;
+    }
+}
